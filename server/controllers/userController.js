f3c6666f56c57c929a45e9003e69bac99710ff1e
chavilla <jcharris.villa@gmail.com>
2020-07-18 14:16:29 -0500

    cambios

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,26 +5,24 @@ const { validationResult } = require("express-validator");
 
 const userController = {
   addUser: async (req, res) => {
-
     //if the validation is false
     const errors = validationResult(req.body);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array()});
+      return res.status(400).json({ errors: errors.array() });
     }
 
     //Destructuing to the req.body
     const { name, email, password, role } = req.body;
 
     try {
-
       //if an user already exists
       const user_already = await User.count({
         where: { email: req.body.email },
       });
 
       if (user_already > 0) {
-        res.json({
-          error: "Ya existe un usuario con este email",
+        res.status(400).json({
+          msg: "Ya existe un usuario con este email",
         });
 
         return;
@@ -36,14 +34,11 @@ const userController = {
       user.dataValues.password = await bcryptjs.hash(password, salt);
       await User.create(user.dataValues);
 
-      res.status(200).json({success: 'Usuario creado con éxito'});
-
+      res.json({ msg: "Usuario creado con éxito" });
     } catch (error) {
-      res.json({
-         error,
-      });
+      res.status(500).json({ msg: "Error en el servidor" });
     }
-  }
+  },
 };
 
 module.exports = userController;
